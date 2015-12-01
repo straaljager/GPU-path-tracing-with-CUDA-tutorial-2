65f32b77a1d733ea30409d60e8060e92e851159c
straaljager <straaljager@users.noreply.github.com>
2015-12-02 10:01:04 +1300

    Update tutorial2_cuda_pathtracer.cu

--- tutorial2_cuda_pathtracer.cu
+++ tutorial2_cuda_pathtracer.cu
@@ -1,4 +1,22 @@
-// Basic CUDA based triangle mesh path tracer by Sam Lapere, 2015 
+/*
+ *  Basic CUDA based triangle mesh path tracer.
+ *  Based on CUDA ray tracing code from http://cg.alexandra.dk/?p=278
+ *  Copyright (C) 2015  Sam Lapere 
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 // Based on Peter Trier's real-time CUDA ray tracer from http://cg.alexandra.dk/?p=278
 
 #include <iostream>
